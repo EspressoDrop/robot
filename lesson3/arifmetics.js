e23011ee9345bfab603e50246b2a60aa7327b032
Andrii Yefimchuk <andrew.yef@gmail.com>
2026-05-12 10:15:08 +0300

    commit for lesson3

---
+++ lesson3/arifmetics.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 5;
+let str1 = "10";
+let str2 = "some string";
+let bul1 = true;
+let num3 = null;
+
+console.log(num1 + num2);
+console.log(num1 * num2);
+console.log(num1 + str1);
+console.log(num2 + bul1);
+console.log(str1 + str2);
+console.log(num3 + num1);
+console.log(bul1 + str2);
---
+++ lesson3/logical.js
@@ -0,0 +1,12 @@
+let age = 25;
+let userName = "John";
+let isLogged = true;
+let level = 1;
+
+console.log(age > 18);
+console.log(age === '25');
+console.log(age == '25');
+
+console.log(age >= 18 && userName === 'John');
+console.log(typeof age === 'number' || typeof userName === 'number');
+console.log(!isLogged);
