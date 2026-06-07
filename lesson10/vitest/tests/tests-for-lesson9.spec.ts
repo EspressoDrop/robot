47be7bf4c0a086aa201443800c3ed9e1ce10355d
Andrii Yefimchuk <andrew.yef@gmail.com>
2026-06-07 23:35:01 +0300

    moved variables into describe and deleted export

--- lesson10/vitest/tests/tests-for-lesson9.spec.ts
+++ lesson10/vitest/tests/tests-for-lesson9.spec.ts
@@ -2,13 +2,12 @@ import { describe, expect, test } from 'vitest';
 import { printDrinkDetails } from '../src/function';
 import { RumBased, ColaBased } from '../src/classes';
 
-export const mojito = new RumBased('Mojito', 10, true, 11, false);
-export const colaCherry = new ColaBased('Coca-Cola Cherry', 5, false, true, 'Cherry', false);
-export const pureRum = new RumBased('Pure Rum', 15, true, 40, true);
-export const dietCola = new ColaBased('Diet Cola', 4, false, true, 'Cola', true);
-
-
 describe('Drink Tests', () => {
+    const mojito = new RumBased('Mojito', 10, true, 11, false);
+    const colaCherry = new ColaBased('Coca-Cola Cherry', 5, false, true, 'Cherry', false);
+    const pureRum = new RumBased('Pure Rum', 15, true, 40, true);
+    const dietCola = new ColaBased('Diet Cola', 4, false, true, 'Cola', true);
+
     test('should print the correct rum-based drink', () => {
         expect(printDrinkDetails(mojito)).to.be.equal('Name: Mojito, Price: $10, Alcohol Content: 11%');
     });
