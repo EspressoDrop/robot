a56779ed45e588f5181e7f136831c2ade0f37876
Andrii Yefimchuk <andrew.yef@gmail.com>
2026-07-21 00:59:30 +0300

    fixes to flaky tests

--- lesson18-19/src/pages/rozetka-main-page.ts
+++ lesson18-19/src/pages/rozetka-main-page.ts
@@ -42,9 +42,10 @@ export class RozetkaMainPage {
 
     public async open() {
         await this.page.goto('https://rozetka.com.ua/', {
-            waitUntil: 'commit'
+            waitUntil: 'domcontentloaded',
+            timeout: 60000
         });
-        await this.rozetkaLogo.waitFor({state: 'visible'});
+        await this.rozetkaLogo.waitFor({state: 'visible', timeout: 30000});
     }
 
     public async openSearch() {
